9db108349c6c5e6bd21418c6813af93fa247f2be
jaguzaro <3013146910101@ingenieria.usac.edu.gt>
2024-12-28 12:03:23 -0600

    integrate visitor tools and update grammar

---
+++ src/grammar/gramatica.pegjs
@@ -0,0 +1,155 @@
+{{
+    import { ids, usos} from '../index.js'
+    import { ErrorReglas } from './error.js';
+    import { errores } from '../index.js'
+    import * as n from '../parser/visitor/CST.js';
+}}
+
+gramatica
+  = _ prods:producciones+ _ {
+    let duplicados = ids.filter((item, index) => ids.indexOf(item) !== index);
+    if (duplicados.length > 0) {
+        errores.push(new ErrorReglas("Regla duplicada: " + duplicados[0]));
+    }
+
+    // Validar que todos los usos están en ids
+    let noEncontrados = usos.filter(item => !ids.includes(item));
+    if (noEncontrados.length > 0) {
+        errores.push(new ErrorReglas("Regla no encontrada: " + noEncontrados[0]));
+    }
+    prods[0].start = true;
+    return prods;
+  }
+
+producciones
+  = _ id:identificador _ alias:$(literales)? _ "=" _ expr:opciones (_";")? {
+    ids.push(id);
+    return new n.Productions(id, expr, alias);
+  }
+
+opciones
+  = expr:union rest:(_ "/" _ @union)* {
+    return new n.Options([expr, ...rest]);
+  }
+
+union
+  = expr:expresion rest:(_ @expresion !(_ literales? _ "=") )* {
+    return new n.Union([expr, ...rest]);
+  }
+
+expresion
+  = label:$(etiqueta/varios)? _ expr:expresiones _ qty:$([?+*]/conteo)? {
+    return new n.Expression(expr, label, qty);
+  }
+
+etiqueta = ("@")? _ id:identificador _ ":" (varios)?
+
+varios = ("!"(!".") /"$"/"@"/"&")
+
+expresiones
+  = id:identificador {
+    usos.push(id);
+    return new n.Identifier(id);
+  }
+  / val:$literales isCase:"i"? {
+    return new n.String(val.replace(/['"]/g, ''), isCase);
+  }
+  / "(" _ opciones:opciones _ ")"
+  / exprs:corchetes isCase:"i"?{
+    return new n.Clase(exprs, isCase);
+
+  }
+  / "." {
+    return new n.Dot(true);
+  }
+  / "!."{
+    return new n.End();
+  }
+
+// conteo = "|" parteconteo _ (_ delimitador )? _ "|"
+
+conteo = "|" _ (numero / id:identificador) _ "|"
+        / "|" _ (numero / id:identificador)? _ ".." _ (numero / id2:identificador)? _ "|"
+        / "|" _ (numero / id:identificador)? _ "," _ opciones _ "|"
+        / "|" _ (numero / id:identificador)? _ ".." _ (numero / id2:identificador)? _ "," _ opciones _ "|"
+
+// parteconteo = identificador
+//             / [0-9]? _ ".." _ [0-9]?
+// 			/ [0-9]
+
+// delimitador =  "," _ expresion
+
+// Regla principal que analiza corchetes con contenido
+corchetes
+    = "[" contenido:(rango / contenido)+ "]" {
+        return contenido;
+    }
+
+// Regla para validar un rango como [A-Z]
+rango
+    = inicio:$caracter "-" fin:$caracter {
+        return new n.Range(inicio, fin);
+    }
+
+// Regla para caracteres individuales
+caracter
+    = [a-zA-Z0-9_ ] 
+
+// Coincide con cualquier contenido que no incluya "]"
+contenido
+    = contenido: (corchete / @$texto){
+        return new n.literalRango(contenido);
+    }
+
+corchete
+    = "[" contenido "]"
+
+texto
+    = "\\" escape
+    /[^\[\]]
+
+literales
+  = '"' @stringDobleComilla* '"'
+  / "'" @stringSimpleComilla* "'"
+
+stringDobleComilla = !('"' / "\\" / finLinea) .
+                    / "\\" escape
+                    / continuacionLinea
+
+stringSimpleComilla = !("'" / "\\" / finLinea) .
+                    / "\\" escape
+                    / continuacionLinea
+
+continuacionLinea = "\\" secuenciaFinLinea
+
+finLinea = [\n\r\u2028\u2029]
+
+escape = "'"
+        / '"'
+        / "\\"
+        / "b"
+        / "f"
+        / "n"
+        / "r"
+        / "t"
+        / "v"
+        / "u"
+
+secuenciaFinLinea = "\r\n" / "\n" / "\r" / "\u2028" / "\u2029"
+
+// literales = 
+//     "\"" [^"]* "\""
+//     / "'" [^']* "'"
+    
+
+numero = [0-9]+
+
+identificador = [_a-z]i[_a-z0-9]i* { return text() }
+
+
+_ = (Comentarios /[ \t\n\r])*
+
+
+Comentarios = 
+    "//" [^\n]* 
+    / "/*" (!"*/" .)* "*/"
---
+++ src/visitor/CST.js
@@ -0,0 +1,255 @@
+
+/**
+ * @template T
+ * @typedef {import('./Visitor.js').default<T>} Visitor
+ */
+/**
+ * @typedef {import('./Node.js').default} Node
+ */
+
+
+/**
+ * @implements {Node}
+ */
+export class Productions {
+    /**
+     *
+     * @param {string} id
+	 * @param {Options} expr
+	 * @param {string=} alias
+	 * @param {boolean=} start
+     */
+    constructor(id, expr, alias, start) {
+        this.id = id;
+		this.expr = expr;
+		this.alias = alias;
+		this.start = start;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitProductions(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Options {
+    /**
+     *
+     * @param {Union[]} exprs
+     */
+    constructor(exprs) {
+        this.exprs = exprs;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitOptions(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Union {
+    /**
+     *
+     * @param {Expression[]} exprs
+     */
+    constructor(exprs) {
+        this.exprs = exprs;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitUnion(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Expression {
+    /**
+     *
+     * @param {Node} expr
+	 * @param {string=} label
+	 * @param {string=} qty
+     */
+    constructor(expr, label, qty) {
+        this.expr = expr;
+		this.label = label;
+		this.qty = qty;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitExpression(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class String {
+    /**
+     *
+     * @param {string} val
+	 * @param {boolean=} isCase
+     */
+    constructor(val, isCase) {
+        this.val = val;
+		this.isCase = isCase;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitString(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Clase {
+    /**
+     *
+     * @param {(string|Range)[]} chars
+	 * @param {boolean=} isCase
+     */
+    constructor(chars, isCase) {
+        this.chars = chars;
+		this.isCase = isCase;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitClase(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Range {
+    /**
+     *
+     * @param {string} bottom
+	 * @param {string} top
+     */
+    constructor(bottom, top) {
+        this.bottom = bottom;
+		this.top = top;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitRange(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Identifier {
+    /**
+     *
+     * @param {string} id
+     */
+    constructor(id) {
+        this.id = id;
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitIdentifier(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class Dot {
+    /**
+     *
+    
+     */
+    constructor() {
+        
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitDot(this);
+    }
+}
+    
+
+/**
+ * @implements {Node}
+ */
+export class End {
+    /**
+     *
+    
+     */
+    constructor() {
+        
+    }
+
+    /**
+     * @template T
+     * @param {Visitor<T>} visitor
+     * @returns {T}
+     */
+    accept(visitor) {
+        return visitor.visitEnd(this);
+    }
+}
+    
